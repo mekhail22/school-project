056aee15d17ca0be2ee00c77b94871232b5e6d1a
mekhail22 <mekhailsaber905@gmail.com>
2025-11-05 01:13:13 +0200

    Add files via upload

--- grade_6.py
+++ grade_6.py
@@ -290,7 +290,7 @@ elif st.session_state.page == "teacher_attendance":
     st.subheader(f"👨‍🏫 المعلم: {teacher_name}")
 
     # MULTISELECT لاختيار الطلاب الغائبين
-    selected = st.multiselect("اختر/ي الطلاب الغائبين (اختار أكثر من طالب لو احتجت):", STUDENTS)
+    selected = st.multiselect("اختر الغائيبين:", STUDENTS)
 
     st.markdown("**اختر نوع الغياب:**")
     col_a, col_b = st.columns(2)
